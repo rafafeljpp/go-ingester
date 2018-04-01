97a15c927d83d7e64ae7482f095bb9fc34c47d7a
Rafael Pellicer <rafael.pellicer@sigis.com.ve>
2018-04-01 09:55:24 -0400

    reorganizando

---
+++ pool/job/job.go
@@ -0,0 +1,12 @@
+package job
+
+import "fmt"
+
+// Job type.
+type Job struct {
+	Payload string
+}
+
+func init() {
+	fmt.Println("rectangle package initialized")
+}
---
+++ pool/worker/worker.go
@@ -0,0 +1,35 @@
+package worker
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Worker type
+type Worker struct {
+	id        int
+	startedAt time.Time
+	messages  chan Job
+	signals   chan string
+	status    string
+}
+
+func (w *Worker) listen(wg *sync.WaitGroup) {
+
+	defer wg.Done()
+	for {
+		select {
+		case msg := <-w.messages:
+			fmt.Println("Mensaje recibido por el worker: ", w.id, msg.Payload)
+			time.Sleep(time.Second * msg.wait)
+			break
+		case <-w.signals:
+			fmt.Println("finalizando el worker ", w.id)
+			w.status = Locked
+			close(w.messages)
+
+			return
+		}
+	}
+}
